ba8215125d6a3f614eef924b7f83e81a3c7720b6
John Lewis <jalewis1221@gmail.com>
2021-03-13 19:19:31 -0600

    changes

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -12,6 +12,6 @@ export const clearToken = () => {
 export const getGoogleToken = () => {};
 
 export const validateEmail = (email) => {
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 };
