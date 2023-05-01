73de2b0de0a44ce019d7e9b47477c651d6d40ece
John Lewis <jalewis1221@gmail.com>
2023-04-30 23:39:39 -0400

    video

--- client/src/components/VideoUploader/VideoUploader.js
+++ client/src/components/VideoUploader/VideoUploader.js
@@ -1,26 +1,55 @@
-import { useEffect, useRef } from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
+import { Loading } from "../../components";
 
-const VideoUploader = () => {
-  const cloudinaryRef = useRef();
-  const widgetRef = useRef();
-  useEffect(() => {
-    cloudinaryRef.current = window.cloudinary;
-    widgetRef.current = cloudinaryRef.current.createUploadWidget(
-      {
-        cloudName: "localmassagepros",
-        uploadPreset: "dlxzn2uj",
-        resourceType: "video",
-        sourceTypes: ["local", "camera"],
-        maxDuration: 60, // set the maximum duration limit to 60 seconds
-        multiple: false,
-        cropping: true,
-      },
-      function (error, result) {
-        console.log(result);
-      }
-    );
-  }, []);
-  return <button onClick={() => widgetRef.current.open()}>Upload Video</button>;
-};
+function VideoUploader() {
+  const [file, setFile] = useState(null);
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleFileChange = (event) => {
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const handleUpload = () => {
+    setSubmitting(true);
+    const reader = new FileReader();
+    reader.onload = async (event) => {
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "dlxzn2uj");
+      formData.append("resource_type", "video");
+      formData.append("max_duration", 60);
+
+      const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/localmassagepros/video/upload`,
+        formData
+      );
+      console.log(response.data);
+    };
+    reader.readAsDataURL(file);
+    setSubmitting(false);
+  };
+
+  return (
+    <div>
+      {submitting ? (
+        <Loading />
+      ) : (
+        <Fragment>
+          <input
+            type="file"
+            accept="video/*"
+            capture="user"
+            onChange={handleFileChange}
+          />
+          <button disabled={submitting} onClick={handleUpload}>
+            Upload
+          </button>
+        </Fragment>
+      )}
+    </div>
+  );
+}
 
 export default VideoUploader;
