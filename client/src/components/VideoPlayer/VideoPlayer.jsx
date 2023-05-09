6d258c64dbb64de49b50d36f0d6f98a31bf05c8d
John Lewis <jalewis1221@gmail.com>
2023-05-08 23:22:46 -0400

    full-screen

--- client/src/components/VideoPlayer/VideoPlayer.jsx
+++ client/src/components/VideoPlayer/VideoPlayer.jsx
@@ -3,13 +3,26 @@ import { useEffect, useRef } from "react";
 const VideoPlayer = ({ publicId, width, height, props, controls }) => {
   const cloudinaryRef = useRef();
   const videoRef = useRef();
+
   useEffect(() => {
     if (cloudinaryRef.current) return;
     cloudinaryRef.current = window.cloudinary;
-    cloudinaryRef.current.videoPlayer(videoRef.current, {
+    const videoPlayer = cloudinaryRef.current.videoPlayer(videoRef.current, {
       cloud_name: "localmassagepros",
     });
+    videoPlayer.on("play", () => {
+      if (videoRef.current.requestFullscreen) {
+        videoRef.current.requestFullscreen();
+      } else if (videoRef.current.webkitRequestFullscreen) {
+        /* Safari */
+        videoRef.current.webkitRequestFullscreen();
+      } else if (videoRef.current.msRequestFullscreen) {
+        /* IE11 */
+        videoRef.current.msRequestFullscreen();
+      }
+    });
   }, []);
+
   return (
     <video
       key={publicId}
