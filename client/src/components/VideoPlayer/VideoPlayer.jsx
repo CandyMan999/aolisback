e108f19718cd827150e84312f2b589a3532cbe52
John Lewis <jalewis1221@gmail.com>
2023-05-08 23:42:21 -0400

    full-screen

--- client/src/components/VideoPlayer/VideoPlayer.jsx
+++ client/src/components/VideoPlayer/VideoPlayer.jsx
@@ -19,6 +19,16 @@ const VideoPlayer = ({ publicId, width, height, props, controls }) => {
       } else if (videoRef.current.msRequestFullscreen) {
         /* IE11 */
         videoRef.current.msRequestFullscreen();
+        // Listen for touchstart event to enter fullscreen mode on mobile
+        const onTouchStart = () => {
+          videoRef.current.requestFullscreen();
+        };
+        videoRef.current.addEventListener("touchstart", onTouchStart);
+
+        // Cleanup touchstart event listener on unmount
+        return () => {
+          videoRef.current.removeEventListener("touchstart", onTouchStart);
+        };
       }
     });
   }, []);
