5097ac07712af6ae5392b2f8966d4c2c67047309
John Lewis <jalewis1221@gmail.com>
2023-05-09 15:56:19 -0400

    hot-load

--- client/src/modules/messageCenter/MessageCenter.jsx
+++ client/src/modules/messageCenter/MessageCenter.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Box } from "../../components";
+import { FETCH_ME } from "../../graphql/queries";
+import { Box, Loading } from "../../components";
 import { useHistory } from "react-router-dom";
 import MessageContainer from "./MessageContainer.jsx";
 import { useClient } from "../../client";
 import Context from "../../context";
+import { getToken } from "../../utils/helpers";
 
 const MessageCenter = () => {
   let history = useHistory();
@@ -11,8 +13,15 @@ const MessageCenter = () => {
   const { state, dispatch } = useContext(Context);
   const currentUser = state.currentUser;
   const { sentVideos, receivedVideos } = currentUser;
-
+  const token = getToken();
+  const [loading, setLoading] = useState(false);
   const [groupedReceived, setGroupReceived] = useState(null);
+
+  useEffect(() => {
+    if (token) {
+      handleFetchMe();
+    }
+  }, [token]);
 
   useEffect(() => {
     if (receivedVideos && !!receivedVideos.length) {
@@ -20,31 +29,61 @@ const MessageCenter = () => {
     }
   }, [receivedVideos]);
 
-  const groupVideosBySender = (videos) => {
-    const groups = {};
+  const handleFetchMe = async () => {
+    try {
+      setLoading(true);
+      const variables = {
+        token,
+      };
 
-    for (const video of videos) {
-      const senderUsername = video.sender.username;
+      const { fetchMe } = await client.request(FETCH_ME, variables);
 
-      if (groups[senderUsername]) {
-        groups[senderUsername].push(video);
-      } else {
-        groups[senderUsername] = [video];
-      }
+      await dispatch({ type: "LOGIN_USER", payload: fetchMe });
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
     }
+  };
 
-    setGroupReceived(Object.values(groups));
+  const groupVideosBySender = (videos) => {
+    try {
+      setLoading(true);
+      const groups = {};
+
+      for (const video of videos) {
+        const senderUsername = video.sender.username;
+
+        if (groups[senderUsername]) {
+          groups[senderUsername].push(video);
+        } else {
+          groups[senderUsername] = [video];
+        }
+      }
+
+      setGroupReceived(Object.values(groups));
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
   };
 
   return (
     <Box width="100%" display="flex" column>
-      <MessageContainer
-        history={history}
-        client={client}
-        dispatch={dispatch}
-        currentUser={currentUser}
-        receivedVideos={groupedReceived}
-      />
+      {loading ? (
+        <Box height="100vH" alignItems="center">
+          <Loading grid size={100} />
+        </Box>
+      ) : (
+        <MessageContainer
+          history={history}
+          client={client}
+          dispatch={dispatch}
+          currentUser={currentUser}
+          receivedVideos={groupedReceived}
+        />
+      )}
     </Box>
   );
 };
