2b9a7f210089c992a13d8bd4e79a19675a5a7d62
John Lewis <jalewis1221@gmail.com>
2023-05-21 19:31:15 -0500

    edit-room

--- client/src/components/CreateRoom/CreateRoom.jsx
+++ client/src/components/CreateRoom/CreateRoom.jsx
@@ -1,74 +1,137 @@
-import React, { Component } from "react";
-import { Input } from "../../components";
+import React, { useState } from "react";
+import { Box, Icon, ICON_SIZES } from "../../components";
 import { motion } from "framer-motion";
+import { COLORS } from "../../constants";
 
-class CreateRoom extends Component {
-  state = {
-    roomName: "",
-    touched: false,
+const CreateRoom = ({ currentUserID, createRoom, dispatch }) => {
+  const [roomName, setRoomName] = useState("");
+  const [touched, setTouched] = useState(false);
+
+  const handleChange = (e) => {
+    setRoomName(e.target.value);
   };
 
-  handleChange = (e) => {
-    this.setState({
-      roomName: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const variables = {
-      name: this.state.roomName,
-      _id: this.props.currentUserID,
+      name: roomName,
+      _id: currentUserID,
     };
-    this.props.createRoom(variables);
-    this.setState({ roomName: "", touched: false });
+    createRoom(variables);
+    setRoomName("");
+    setTouched(false);
   };
 
-  handleIsLoggedIn = async () => {
-    try {
-      if (!this.props.currentUserID) {
-        this.props.dispatch({ type: "TOGGLE_SIGNUP", payload: true });
-      } else {
-        this.setState({ touched: !this.state.touched });
-      }
-    } catch (err) {
-      console.log(err);
+  const handleIsLoggedIn = () => {
+    if (!currentUserID) {
+      dispatch({ type: "TOGGLE_SIGNUP", payload: true });
+    } else {
+      setTouched(!touched);
     }
   };
 
-  render() {
-    return (
-      <motion.div
-        onClick={() => this.handleIsLoggedIn()}
-        className="new-room-form"
-        animate={{ width: this.state.touched ? "50vW" : "100%" }}
-        transition={{ ease: "linear", duration: 0.5 }}
-        style={{ border: "1px solid #69ffb4" }}
+  const inputContainerVariants = {
+    hidden: {
+      width: "0%",
+    },
+    visible: {
+      width: "40vW",
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      onClick={!touched ? handleIsLoggedIn : undefined}
+      className="new-room-form"
+      animate={{ width: touched ? "50vW" : "100%" }}
+      transition={{ ease: "linear", duration: 0.5 }}
+    >
+      <Box
+        onClick={handleIsLoggedIn}
+        boxShadow={`1px 1px 1px 2px rgba(0, 0, 0, 0.2)`}
+        style={{
+          position: "absolute",
+          bottom: "20px",
+          right: "20px",
+          borderRadius: "50%",
+          backgroundColor: COLORS.white,
+          height: "60px",
+          width: "60px",
+          display: touched ? "none" : undefined,
+        }}
       >
-        <form onSubmit={this.handleSubmit}>
-          <Input
-            style={{
-              height: "100%",
-              width: "100%",
-            }}
-            fontSize={"10px"}
-            withoutFormik
-            value={this.state.roomName}
-            onChange={this.handleChange}
-            type="text"
-            placeholder="Create Room"
-            required
-            autoFocus={false}
+        <Box display="flex" width="100%" justifyContent="center">
+          <Icon
+            onClick={() => setTouched(false)}
+            name="pencil"
+            size={ICON_SIZES.X_LARGE}
+            color={COLORS.grey}
           />
-          {!!this.state.roomName.length && (
-            <button id="create-room-btn" type="submit">
-              +
-            </button>
-          )}
-        </form>
+        </Box>
+      </Box>
+      <motion.div
+        initial="hidden"
+        animate={touched ? "visible" : "none"}
+        variants={inputContainerVariants}
+        style={{
+          position: "absolute",
+          bottom: "40px",
+          right: "5%",
+        }}
+      >
+        {touched && (
+          <form onSubmit={handleSubmit}>
+            <Box
+              position="absolute"
+              top={-12}
+              left={-12}
+              backgroundColor={COLORS.white}
+              border={`solid 1px ${COLORS.grey}`}
+              boxShadow={`1px 1px 1px 2px rgba(0, 0, 0, 0.2)`}
+              borderRadius={"50%"}
+              padding={3}
+            >
+              <Icon
+                onClick={() => setTouched(false)}
+                name="close"
+                size={ICON_SIZES.MEDIUM}
+                color={COLORS.grey}
+              />
+            </Box>
+
+            <input
+              style={{
+                width: "100%",
+                borderBottom: "none",
+                borderRadius: "20px",
+                height: "30px",
+              }}
+              fontSize={"10px"}
+              value={roomName}
+              onChange={handleChange}
+              type="text"
+              placeholder="Create Room"
+              required
+            />
+
+            {!!roomName.length && (
+              <Box position="absolute" right={4} top={3}>
+                <Icon
+                  onClick={handleSubmit}
+                  name="send"
+                  size={ICON_SIZES.X_LARGE}
+                  color={COLORS.vividBlue}
+                />
+              </Box>
+            )}
+          </form>
+        )}
       </motion.div>
-    );
-  }
-}
+    </motion.div>
+  );
+};
 
 export default CreateRoom;
