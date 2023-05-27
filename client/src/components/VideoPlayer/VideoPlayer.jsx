335209a450e11c912697f9150a21d36eeca48e34
John Lewis <jalewis1221@gmail.com>
2023-05-27 09:43:01 -0500

    test

--- client/src/components/VideoPlayer/VideoPlayer.jsx
+++ client/src/components/VideoPlayer/VideoPlayer.jsx
@@ -12,6 +12,7 @@ const VideoPlayer = ({
 }) => {
   const cloudinaryRef = useRef();
   const videoRef = useRef();
+  let element;
 
   useEffect(() => {
     if (cloudinaryRef.current) return;
@@ -21,19 +22,85 @@ const VideoPlayer = ({
     });
 
     videoPlayer.on("play", () => {
-      videoPlayer.maximize();
+      if (fullScreen) {
+        videoPlayer.maximize();
+      }
     });
 
     videoPlayer.on("fullscreenchange", () => {
       const maxView = videoPlayer.isMaximized();
       if (!maxView) {
         videoPlayer.exitMaximize();
+
+        document
+          .exitFullscreen()
+          .then(() => console.log("Document Exited from Full screen mode"))
+          .catch((err) => console.error(err));
       }
     });
+
+    function fullscreenchanged(event) {
+      if (document.fullscreenElement) {
+        console.log(
+          `Element: ${document.fullscreenElement.id} entered fullscreen mode.`
+        );
+        element = document.fullscreenElement.id;
+      } else {
+        const videoElement = document.getElementById(element);
+        console.log("!!: ", videoElement);
+
+        if (videoElement) {
+          if (videoElement.exitFullscreen) {
+            videoElement
+              .exitFullscreen()
+              .then(() => console.log("Document Exited from Full screen mode"))
+              .catch((err) => console.error(err));
+          } else if (videoElement.mozCancelFullScreen) {
+            videoElement
+              .mozCancelFullScreen()
+              .then(() => console.log("Document Exited from Full screen mode"))
+              .catch((err) => console.error(err));
+          } else if (videoElement.webkitExitFullscreen) {
+            videoElement
+              .webkitExitFullscreen()
+              .then(() => console.log("Document Exited from Full screen mode"))
+              .catch((err) => console.error(err));
+          } else if (videoElement.msExitFullscreen) {
+            videoElement
+              .msExitFullscreen()
+              .then(() => console.log("Document Exited from Full screen mode"))
+              .catch((err) => console.error(err));
+          }
+        }
+
+        if (document.exitFullscreen) {
+          document
+            .exitFullscreen()
+            .then(() => console.log("Document Exited from Full screen mode"))
+            .catch((err) => console.error(err));
+        } else if (document.mozCancelFullScreen) {
+          document
+            .mozCancelFullScreen()
+            .then(() => console.log("Document Exited from Full screen mode"))
+            .catch((err) => console.error(err));
+        } else if (document.webkitExitFullscreen) {
+          document
+            .webkitExitFullscreen()
+            .then(() => console.log("Document Exited from Full screen mode"))
+            .catch((err) => console.error(err));
+        } else if (document.msExitFullscreen) {
+          document
+            .msExitFullscreen()
+            .then(() => console.log("Document Exited from Full screen mode"))
+            .catch((err) => console.error(err));
+        }
+      }
+    }
+
+    document.addEventListener("fullscreenchange", fullscreenchanged);
   }, []);
 
   return (
-    // <Box key={`${publicId}-container`} height={"auto"} width="auto">
     <video
       key={publicId}
       ref={videoRef}
@@ -44,7 +111,6 @@ const VideoPlayer = ({
       style={{ borderRadius: borderRadius ? borderRadius : undefined }} // Add border radius
       {...props}
     />
-    // </Box>
   );
 };
 
