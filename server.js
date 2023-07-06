4ea07931619c361aaa58d9bd862630b2d0d2512d
John Lewis <jalewis1221@gmail.com>
2023-07-06 11:55:47 -0500

    fuck

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ const startServer = async () => {
     typeDefs,
     resolvers,
     introspection: true,
-    cors: {
-      origin: ["https://www.gonechatting.com", "http://localhost:3000"], // Replace with your client's origin
-      credentials: true,
-    },
+    // cors: {
+    //   origin: ["https://www.gonechatting.com", "http://localhost:3000"], // Replace with your client's origin
+    //   credentials: true,
+    // },
     playground: true,
     context: async ({ req, connection }) => {
       if (connection) {
