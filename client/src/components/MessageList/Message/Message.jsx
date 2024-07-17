3b1f85bfac65895f9f7d2f2a969553620d1429da
John Lewis <jalewis1221@gmail.com>
2024-07-17 16:46:28 -0500

    homescreen message UI

--- client/src/components/MessageList/Message/Message.jsx
+++ client/src/components/MessageList/Message/Message.jsx
@@ -1,8 +1,61 @@
-import React, { Fragment } from "react";
-import { motion } from "framer-motion";
-import { Box, Text, FONT_SIZES, Picture, Button } from "../../../components";
+import React from "react";
+import styled from "@emotion/styled";
+import { Box, Text, FONT_SIZES, Button, Picture } from "../../../components";
 import { COLORS } from "../../../constants";
 import { formatDistanceToNow } from "date-fns";
+import { motion } from "framer-motion";
+
+const MessageRow = styled.div`
+  display: flex;
+  align-items: flex-start;
+  width: "100%";
+`;
+
+const MessageBubble = styled.div`
+  background-color: ${(props) =>
+    props.isCurrentUser ? COLORS.main : COLORS.lightGrey};
+  color: ${(props) => (props.isCurrentUser ? COLORS.white : COLORS.black)};
+  border-radius: ${(props) =>
+    props.isCurrentUser ? "15px 15px 0 15px" : "15px 15px 15px 0"};
+
+  padding-left: 10px;
+  padding-right: 10px;
+  box-shadow: 2px 2px 4px 2px rgba(0, 0, 0, 0.3);
+
+  display: flex;
+  flex-direction: column;
+  margin-left: ${(props) => (props.isCurrentUser ? "0" : "10px")};
+  margin-right: ${(props) => (props.isCurrentUser ? "10px" : "0")};
+`;
+
+const AvatarContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 60px;
+  height: 55px;
+  margin-top: 30px;
+  cursor: pointer;
+`;
+
+const Username = styled(Text)`
+  font-size: 20px;
+  color: ${COLORS.vividBlue};
+  margin-bottom: 2px;
+  font-weight: bold;
+  margin-top: 2px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const Timestamp = styled(Text)`
+  font-size: 10px;
+  color: ${COLORS.darkGrey};
+  align-self: flex-end;
+  margin-top: 5px;
+  margin: 0,
+  padding: 0
+`;
 
 const Message = ({
   username,
@@ -19,194 +72,88 @@ const Message = ({
     try {
       window.speechSynthesis.speak(new SpeechSynthesisUtterance(text));
     } catch (err) {
-      console.log("error talking:  ", err);
+      console.log("error talking: ", err);
     }
   };
 
+  const isCurrentUser = currentUser === authorId;
+
   return (
-    <Fragment>
-      {roomId === messageRoomId &&
-        (currentUser === authorId ? (
-          <Box
-            display={"flex"}
-            justifyContent={"flex-end"}
-            height={"fit-content"}
-            minHeight={55}
-            className="message"
-            margin="auto"
-            flexWrap="wrap"
-            paddingTop={10}
-            marginLeft={2}
-            marginRight={2}
-          >
-            <Box
-              flexWrap="wrap"
-              height={"fit-content"}
-              marginRight={50}
-              className="message-text"
-            >
-              <Text
-                paddingLeft={2}
-                paddingRight={2}
-                fontSize={FONT_SIZES.SMALL}
-                marginY={"auto"}
-              >
-                <Button
-                  style={{
-                    border: `solid 1px ${COLORS.white}`,
-                    borderRadius: 6,
-                    width: 12,
-                    height: 22,
-                    padding: 6,
-                    margin: 0,
-                    marginRight: 4,
-                    backgroundColor: COLORS.lightGrey,
-                  }}
-                  onClick={() => handleSpeech(text, "male")}
-                />
-                {text}
-                <Text
-                  fontSize={FONT_SIZES.X_SMALL}
-                  color={COLORS.darkGrey}
-                  style={{
-                    width: "100%",
-                    justifyContent: "flex-start",
-                    display: "flex",
-                  }}
-                >
-                  {formatDistanceToNow(Number(createdAt))} ago
-                </Text>
-              </Text>
-            </Box>
-            <motion.div
+    <MessageRow
+      isCurrentUser={isCurrentUser}
+      style={{
+        display: "flex",
+        justifyContent: isCurrentUser ? "flex-end" : "flex-start",
+        paddingLeft: "2%",
+        paddingRight: "2%",
+      }}
+    >
+      {!isCurrentUser && (
+        <AvatarContainer onClick={() => usernameClick(authorId)}>
+          <Picture
+            withShadow
+            profilePic={picture}
+            name={picture._id}
+            height={65}
+            width={65}
+          />
+        </AvatarContainer>
+      )}
+      <div>
+        <Username
+          style={{
+            justifyContent: !isCurrentUser ? "flex-start" : "flex-end",
+            marginRight: isCurrentUser ? "8px" : undefined,
+            marginLeft: !isCurrentUser ? "8px" : undefined,
+
+            textShadow: `-0.5px -0.5px 0 ${COLORS.vividBlue}, 1px -0.5px 0 ${COLORS.vividBlue}, -0.5px 0.5px 0 ${COLORS.vividBlue}, 0.5px 0.5px 0 ${COLORS.vividBlue}`,
+            color: COLORS.black,
+          }}
+        >
+          {username}
+        </Username>
+
+        <MessageBubble isCurrentUser={isCurrentUser}>
+          <Text fontSize={FONT_SIZES.SMALL}>
+            <Button
               style={{
-                width: 45,
-                position: "absolute",
-                display: "flex",
-                flexWrap: "wrap",
-                height: 35,
-                paddingLeft: 1,
-                paddingRight: 1,
-                textAlign: "center",
-                justifyContent: "center",
+                border: `solid 1px ${COLORS.white}`,
+                borderRadius: 6,
+                width: 12,
+                height: 22,
+                padding: 6,
+                margin: 0,
+                marginRight: 4,
+                backgroundColor: isCurrentUser ? COLORS.pink : COLORS.main,
               }}
-              whileHover={{
-                scale: 1.9,
-                zIndex: 2000,
-                cursor: "pointer",
-                translateX: -20,
-              }}
-              onClick={() => usernameClick(authorId)}
-            >
-              <Picture
-                profilePic={picture}
-                name={picture._id}
-                height={45}
-                width={45}
-              />
-
-              <Text
-                margin={0}
-                fontSize={FONT_SIZES.X_SMALL}
-                className="message-username"
-                style={{ marginBottom: 5 }}
-              >
-                {username}
-              </Text>
-            </motion.div>
-          </Box>
-        ) : (
-          <Box
-            display={"flex"}
-            height={"fit-content"}
-            className="message"
-            minHeight={55}
-            margin="auto"
-            flexWrap="wrap"
-            paddingTop={10}
-            marginRight={2}
-            marginLeft={3}
+              onClick={() =>
+                handleSpeech(text, isCurrentUser ? "male" : "female")
+              }
+            />
+            {text}
+          </Text>
+          <Timestamp
+            style={{
+              width: !isCurrentUser ? "100%" : undefined,
+              color: isCurrentUser ? COLORS.darkerGrey : COLORS.white,
+            }}
           >
-            <motion.div
-              style={{
-                width: 45,
-                position: "absolute",
-                display: "flex",
-                flexWrap: "wrap",
-                height: 35,
-
-                textAlign: "center",
-                justifyContent: "center",
-              }}
-              whileHover={{
-                scale: 1.9,
-                zIndex: 2000,
-                cursor: "pointer",
-                translateX: 20,
-              }}
-              onClick={() => usernameClick(authorId)}
-            >
-              <Picture
-                profilePic={picture}
-                name={picture._id}
-                height={45}
-                width={45}
-              />
-              <Text
-                margin={0}
-                fontSize={FONT_SIZES.X_SMALL}
-                className="message-username"
-              >
-                {username}
-              </Text>
-            </motion.div>
-
-            <Box
-              flexWrap="wrap"
-              height={"fit-content"}
-              className="others-message-text"
-              marginLeft={50}
-            >
-              <Text
-                paddingLeft={2}
-                paddingRight={2}
-                fontSize={FONT_SIZES.SMALL}
-                marginY="auto"
-              >
-                <Button
-                  style={{
-                    border: `solid 1px ${COLORS.white}`,
-                    borderRadius: 6,
-                    width: 14,
-                    height: 22,
-                    padding: 6,
-                    margin: 0,
-                    marginRight: 4,
-                    backgroundColor: COLORS.main,
-                  }}
-                  onClick={() => handleSpeech(text, "female")}
-                />
-
-                {text}
-              </Text>
-
-              <Text
-                paddingLeft={4}
-                fontSize={FONT_SIZES.X_SMALL}
-                color={COLORS.darkGrey}
-                marginRight={2}
-                style={{
-                  width: "100%",
-                  justifyContent: "flex-end",
-                  display: "flex",
-                }}
-              >
-                {formatDistanceToNow(Number(createdAt))} ago
-              </Text>
-            </Box>
-          </Box>
-        ))}
-    </Fragment>
+            {formatDistanceToNow(Number(createdAt))} ago
+          </Timestamp>
+        </MessageBubble>
+      </div>
+      {isCurrentUser && (
+        <AvatarContainer onClick={() => usernameClick(authorId)}>
+          <Picture
+            withShadow
+            profilePic={picture}
+            name={picture._id}
+            height={65}
+            width={65}
+          />
+        </AvatarContainer>
+      )}
+    </MessageRow>
   );
 };
 
