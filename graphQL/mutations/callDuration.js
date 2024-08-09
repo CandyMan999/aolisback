e0705e968f8f9117c0e54cbb187b9005f9327d95
John Lewis <jalewis1221@gmail.com>
2024-08-09 18:12:05 -0500

    out of minutes fix

--- graphQL/mutations/callDuration.js
+++ graphQL/mutations/callDuration.js
@@ -7,6 +7,7 @@ module.exports = {
     try {
       const userTime = await User.findById({ _id: userID });
 
+      // Check if the user still has videoMinutes available
       if (userTime.plan.videoMinutes > userTime.plan.videoMinutesUsed) {
         const user = await User.findByIdAndUpdate(
           { _id: userID },
@@ -20,12 +21,11 @@ module.exports = {
         return { user, outOfTime: false };
       }
 
-      // Check if videoMinutesUsed exceeds videoMinutes
+      // Check if videoMinutes are exhausted and additionalMinutes are available
       if (
         userTime.plan.videoMinutes <= userTime.plan.videoMinutesUsed &&
         userTime.plan.additionalMinutes > 0
       ) {
-        // Decrement additionalMinutes if videoMinutes are exceeded
         const user = await User.findByIdAndUpdate(
           { _id: userID },
           {
@@ -36,14 +36,23 @@ module.exports = {
           { new: true }
         ).populate(["pictures", "blockedUsers"]);
 
-        // If additionalMinutes are depleted, return outOfTime
+        // If additionalMinutes are depleted after decrementing
         if (user.plan.additionalMinutes <= 0) {
           return { user, outOfTime: true };
         } else {
           return { user, outOfTime: false };
         }
       }
-      return { user: userTime, outOfTime: false };
+
+      // If both videoMinutes and additionalMinutes are exhausted
+      if (
+        userTime.plan.videoMinutes <= userTime.plan.videoMinutesUsed &&
+        userTime.plan.additionalMinutes <= 0
+      ) {
+        return { user: userTime, outOfTime: true };
+      }
+
+      return { user: userTime, outOfTime: false }; // Default return if no conditions are met
     } catch (err) {
       throw new AuthenticationError(err.message);
     }
