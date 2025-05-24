6537e086dd0b969c8b3b083a8887abe56212bfed
John Lewis <jalewis1221@gmail.com>
2025-05-24 14:59:48 -0500

    changed heroku server name

--- client/src/client.jsx
+++ client/src/client.jsx
@@ -3,7 +3,7 @@ import { getToken } from "./utils/helpers";
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://aolisback.herokuapp.com/graphql"
+    ? "https://gone-chatting-03f3321e7ebb.herokuapp.com"
     : "http://localhost:4000/graphql";
 
 export const useClient = () => {
