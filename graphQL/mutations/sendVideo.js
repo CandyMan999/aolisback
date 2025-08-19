70c255e37eddc180f5235b947ddf8e7d8957f9e8
John Lewis <jalewis1221@gmail.com>
2025-08-19 14:42:18 -0500

    mp4

--- graphQL/mutations/sendVideo.js
+++ graphQL/mutations/sendVideo.js
@@ -105,6 +105,88 @@ module.exports = {
       throw new AuthenticationError(payload);
     }
   },
+
+  sendVideoResolver: async (root, args, ctx) => {
+    const { url, publicId, receiverID, senderID } = args;
+
+    try {
+      // Create the video entry first
+      const video = await Video.create({
+        url,
+        publicId,
+        sender: senderID,
+        receiver: receiverID,
+        flagged: false, // Default to false, update later if nudity is detected
+      });
+
+      // Add the video to the processing queue
+      addToQueue(video._id, url);
+
+      // Update the sender and receiver with the new video
+      const sender = await User.findByIdAndUpdate(
+        { _id: senderID },
+        { $push: { sentVideos: video } },
+        { new: true }
+      ).populate("sentVideos");
+
+      const receiver = await User.findByIdAndUpdate(
+        { _id: receiverID },
+        { $push: { receivedVideos: video } },
+        { new: true }
+      ).populate("receivedVideos");
+
+      // Handle message allowance
+      const user = await User.findById(senderID);
+      if (user.plan.messagesSent < user.plan.messages) {
+        await User.findByIdAndUpdate(
+          { _id: senderID },
+          { $inc: { "plan.messagesSent": 1 } },
+          { new: true }
+        );
+      } else if (
+        user.plan.messagesSent >= user.plan.messages &&
+        user.plan.additionalMessages > 0
+      ) {
+        await User.findByIdAndUpdate(
+          { _id: senderID },
+          { $inc: { "plan.additionalMessages": -1 } },
+          { new: true }
+        );
+      }
+
+      // Populate the new video with sender and receiver details
+      const newVideo = await Video.findOne({ _id: video._id }).populate([
+        {
+          path: "sender",
+          model: "User",
+          populate: {
+            path: "pictures",
+            model: "Picture",
+          },
+        },
+        {
+          path: "receiver",
+          model: "User",
+          populate: {
+            path: "pictures",
+            model: "Picture",
+          },
+        },
+      ]);
+
+      // Send a push notification to the receiver
+      if (receiver && receiver.expoToken) {
+        sendPushNotification(receiver.expoToken, sender.username);
+      }
+
+      // Publish the new video for subscriptions
+      publishCreateVideo(newVideo);
+
+      return newVideo;
+    } catch (err) {
+      throw new AuthenticationError(err.message);
+    }
+  },
 };
 
 // Function to add video processing to the queue
