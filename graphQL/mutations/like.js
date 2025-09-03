98fb6e1e9a0ec0450374847402bff0c404465341
JOHN A LEWIS <jalewis1221@gmail.com>
2025-09-03 17:23:17 -0500

    Fix like resolver to cast ids and allow multiple likes

--- graphQL/mutations/like.js
+++ graphQL/mutations/like.js
@@ -1,4 +1,5 @@
 const { AuthenticationError } = require("apollo-server");
+const mongoose = require("mongoose");
 const { User, Like, Match } = require("../../models");
 const { pushNotificationNewMatch } = require("../../utils/middleware");
 
@@ -7,36 +8,49 @@ module.exports = {
     const { userID, likeID } = args;
 
     try {
-      const user = await User.findById(userID);
+      if (!likeID) throw new Error("Invalid target user");
+
+      const targetId = new mongoose.Types.ObjectId(likeID);
+      const likerId = new mongoose.Types.ObjectId(userID);
+
+      const user = await User.findById(likerId);
       if (!user) throw new Error("User not found");
 
-      const already = await Like.findOne({ user: userID, target: likeID });
+      const already = await Like.findOne({ user: likerId, target: targetId });
       if (already) {
-        const populated = await User.findById(userID).populate(["pictures", "blockedUsers"]);
+        const populated = await User.findById(likerId).populate([
+          "pictures",
+          "blockedUsers",
+        ]);
         return { user: populated, isMatch: false, matchID: likeID };
       }
 
       if (user.plan.likesSent < user.plan.likes) {
-        await User.findByIdAndUpdate(userID, { $inc: { "plan.likesSent": 1 } });
+        await User.findByIdAndUpdate(likerId, { $inc: { "plan.likesSent": 1 } });
       } else if (user.plan.additionalLikes > 0) {
-        await User.findByIdAndUpdate(userID, { $inc: { "plan.additionalLikes": -1 } });
+        await User.findByIdAndUpdate(likerId, {
+          $inc: { "plan.additionalLikes": -1 },
+        });
       } else {
         throw new AuthenticationError("No likes remaining");
       }
 
-      await Like.create({ user: userID, target: likeID });
+      await Like.create({ user: likerId, target: targetId });
 
-      const reciprocal = await Like.findOne({ user: likeID, target: userID });
+      const reciprocal = await Like.findOne({
+        user: targetId,
+        target: likerId,
+      });
       let isMatch = false;
       if (reciprocal) {
         isMatch = true;
-        const users = [userID, likeID].sort();
+        const users = [likerId, targetId].sort();
         await Match.create({ users });
-        const likedUser = await User.findById(likeID);
+        const likedUser = await User.findById(targetId);
         pushNotificationNewMatch(user.username, likedUser.expoToken);
       }
 
-      const populated = await User.findById(userID).populate([
+      const populated = await User.findById(likerId).populate([
         "pictures",
         "blockedUsers",
       ]);
@@ -49,11 +63,14 @@ module.exports = {
   unLikeResolver: async (root, args) => {
     const { userID, unLikeID } = args;
     try {
-      await Like.deleteOne({ user: userID, target: unLikeID });
-      await Like.deleteOne({ user: unLikeID, target: userID });
-      await Match.deleteOne({ users: { $all: [userID, unLikeID] } });
+      const targetId = new mongoose.Types.ObjectId(unLikeID);
+      const likerId = new mongoose.Types.ObjectId(userID);
 
-      const user = await User.findById(userID).populate([
+      await Like.deleteOne({ user: likerId, target: targetId });
+      await Like.deleteOne({ user: targetId, target: likerId });
+      await Match.deleteOne({ users: { $all: [likerId, targetId] } });
+
+      const user = await User.findById(likerId).populate([
         "pictures",
         "blockedUsers",
       ]);
